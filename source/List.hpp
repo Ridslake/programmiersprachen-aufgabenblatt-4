588058df5a65fdfdbd4a70ff7a80db77c880ac3f
Ridslake <Ridslake@googlemail.com>
2016-06-06 10:23:06 +0200

    hue

--- source/List.hpp
+++ source/List.hpp
@@ -63,23 +63,39 @@ struct ListIterator
 	return *this; //iterator points at the next orignial node
   }
 
-//iterator +1, not or.
+//value, before it+1
   Self operator++(int)
   {
 	Self tmp= *this;
 	++(*this);
 	return tmp; //iterator
   }
-
-  bool operator==(const Self& x) const {}
-  bool operator!=(const Sekf& x) const {}
+//node same value as x?
+  bool operator==(const Self& x) const
+  {
+  	return m_node == x.m_node;
+  }
+//node diffrent value
+  bool operator!=(const Self& x) const
+  {
+	return m_node != x.m_node;
+  }
+//next
   Self next() const
   {
-	if(m_node)
+	if(m_node)	//if node
 	  return ListIterator(m_node->m_next);
-	else
+	else		//list ends
 	  return ListIterator(nullptr);
   } 
+//previous
+  Self previous() const
+  {
+  if(m_node)
+	return ListIterator(m_node->m_prev);
+  else
+	return ListIterator(nullptr)
+  }
 
   private:
   //The Node the iterator is pointing to
@@ -89,27 +105,26 @@ struct ListIterator
 template <typename T>
 struct ListConstIterator
 {
-	friend class List <T>;
-	public:
-	//not implemented yet
-	private
-		ListNode<T>*m_node = nullptr;
+  friend class List <T>;
+	
+  public:
+  ListNode<T>*m_node = nullptr;
 };
 
 template <typename T>
 class List
 {
 public:
-	typedef T value_type;
-	typedef T* pointer;
-	typedef const T* const_pointer
-	typedef T& reference;
-	typedef const T& const_reference;
-	typedef ListIterator<T> iterator;
-	typedef ListConstIterator<T> const_interator;
+  typedef T value_type;
+  typedef T* pointer;
+  typedef const T* const_pointer
+  typedef T& reference;
+  typedef const T& const_reference;
+  typedef ListIterator<T> iterator;
+  typedef ListConstIterator<T> const_interator;
 
-	friend class ListIterator<T>;
-	friend class ListConstIterator<T>;
+  friend class ListIterator<T>;
+  friend class ListConstIterator<T>;
 
 //Default Constructor, empty list
   List():
@@ -118,38 +133,91 @@ public:
 	m_last{nullptr} {}
 
 //Copy Constructor
-  List<T>(List<T> const& inlist):
-	
-	
-	bool empty() const;
+  List<T>(List<T> const& thelist):
+  m_size(0),
+  m_first(nullptr),
+  m_last(nullptr) 
+  {
+	for(auto i = thelist.begin(); it != thelist.end(); i++)
 	{
-	  return m_size == 0;	//check empty
+	  push_back(*i);
+	}
+  }
+
+//Move Constructor
+  List<T>(List<T> && thelist) :
+  //adapt thelist 
+  m_size(thelist.m_size),
+  m_first(thelist.m_first),
+  m_last(thelist.m_last)
+  {
+	thelist.m_first = nullptr;
+	thelist.m_last = nullptr;
+	thelist.m_size = 0;
+  }
+//Destructor?
+  ~List(){clear();}
+	
+  bool empty() const;
+  {
+	return m_size == 0;	//check if empty
+  }
+
+  std::size_t size() const;
+  {
+	return m_size;	//returns size
+  }
+
+  T const&  front() const
+  {
+	return m_first->m_value;
+  }
+	
+  T& front
+  {
+	return m_first->m_value;
+  }
+
+  T const& back() const
+  {
+	return m_last->m_value;
+  }
+	
+  T& back
+  {
+	return m_last->m_value;
+  }
+
+//4.8
+  void insert(T const& a, iterator const& p)
+  {
+  iterator i = begin();
+	while(i != p) i++;
+
+	T temp = *i;
+	*i = a;
+	i++;
+
+	while(i!=end())
+	{
+	  T temp2 = *i;
+	  *i = temp;
+	  temp = temp2;
+	  i++;
 	}
 
-	std::size_t size() const;
-	{
-	  return m_size;	//returns size
-	}
+	push_back(temp);
+  }
 
-	T const&  front() const
-	{
-	  return m_first->m_value;
-	}
-	
-	T& front
-	{
-	  return m_first->m_value;
-	}
-
-	T const& back() const
-	{
-	  return m_last->m_value;
-	}
-	
-	T& back
-	{
-	  return m_last->m_value;
-  	}
+//4.9
+  void reverse()
+  {
+  List<T> rev{*this}; //copy der liste
+	clear(); //macht ganze Liste leer
+ 
+	for(iterator i = rev.begin(); i!=rev.end(); ++i)
+	  push_front(*i);
+  }
 
 //Insert a new element at the beginnning of the list, right before its current first element
   void push_front(T const& e)
@@ -225,9 +293,22 @@ public:
 	}
   }
 
+//4.5
+  iterator begin() const
+  {
+  return m_first; //returns iterator at the first node
+  }
+
+  iterator end() const
+  {
+  return ListIterator<T>{};
+  }
+
+//clear list
   void clear()
   {
-	for(int i = 0; i <= m_size; ++i)
+	while(m_size > 0)
+		pop_back();
   }
 
 	private:
@@ -237,5 +318,37 @@ public:
 
 };
 
+//4.6
+template<typename T>
+bool operator == (List<T> const& xs, List<T> const& ys)
+{
+	
+  if(xs.size() != ys.size()) //same size?
+	return false;
+
+  else
+  {
+
+	ListIterator<T> x = xs.begin(); 
+	ListIterator<T> y = ys.begin();
+
+	while(x != xs.end() and y!=ys.end())
+	{
+	  if(*x != *y)
+		return false;
+
+			x++;
+			y++;
+	}
+	return true;
+  }
+}
+
+template<typename T>
+bool operator != (List<T> const& xs, List<T> const& ys)
+{
+  return !(xs == ys);
+}
+		
 #endif //#define BUW_LIST_HPP
 
